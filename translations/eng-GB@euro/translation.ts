d71e0213b0603d1af0455c136d8637a842508518
Luca Realdi <luca.realdi@opencontent.it>
2025-02-07 01:12:42 +0100

    Add eng-GB@euro

---
+++ translations/eng-GB@euro/translation.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+  <context>
+    <name>octranslate</name>
+    <message>
+      <source>Translator settings</source>
+      <translation>Machine translation settings</translation>
+    </message>
+    <message>
+      <source>A %locale% translation already exists for the selected content</source>
+      <translation>A translation in %locale% already exists for the selected content</translation>
+    </message>
+    <message>
+      <source>Manually edit the current translation</source>
+      <translation>Manually edit the current translation</translation>
+    </message>
+    <message>
+      <source>Regenerate the translation automatically and edit the content for review</source>
+      <translation>Automatically regenerate the translation and review the content before publishing it
+            </translation>
+    </message>
+    <message>
+      <source>Create a new %locale% translation for the selected content</source>
+      <translation>Create a new translation in %locale% for the selected content</translation>
+    </message>
+    <message>
+      <source>Automatically translate the content and publish it</source>
+      <translation>Automatically translate and publish the content</translation>
+    </message>
+    <message>
+      <source>Automatically translate without publishing and open the content for review</source>
+      <translation>Automatically translate and review the content before publishing it</translation>
+    </message>
+    <message>
+      <source>Translate attachments</source>
+      <translation>Translate attachments</translation>
+    </message>
+    <message>
+      <source>This content is translated with an automatic translation tool: the text may contain inaccurate information.</source>
+      <translation>This content is translated with an automatic translation tool: the text may contain inaccurate information.</translation>
+    </message>
+  </context>
+</TS>
