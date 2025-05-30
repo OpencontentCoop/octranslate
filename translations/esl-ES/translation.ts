ea2ac503a630363766501068ff9842f66d441a50
Luca Realdi <luca.realdi@opencontent.it>
2025-05-30 17:37:09 +0200

    Add por-BR and esl-ES translations

---
+++ translations/esl-ES/translation.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es">
+  <context>
+    <name>octranslate</name>
+    <message>
+      <source>Translator settings</source>
+      <translation>Ajustes del traductor automático</translation>
+    </message>
+    <message>
+      <source>A %locale% translation already exists for the selected content</source>
+      <translation>Ya existe una traducción en %locale% para el contenido seleccionado</translation>
+    </message>
+    <message>
+      <source>Manually edit the current translation</source>
+      <translation>Modificar manualmente la traducción actual</translation>
+    </message>
+    <message>
+      <source>Regenerate the translation automatically and edit the content for review</source>
+      <translation>Regenera automáticamente la traducción y revisa el contenido antes de publicarlo
+            </translation>
+    </message>
+    <message>
+      <source>Create a new %locale% translation for the selected content</source>
+      <translation>Crea una nueva traducción en %locale% para el contenido seleccionado</translation>
+    </message>
+    <message>
+      <source>Automatically translate the content and publish it</source>
+      <translation>Traduce automáticamente y publica el contenido</translation>
+    </message>
+    <message>
+      <source>Automatically translate without publishing and open the content for review</source>
+      <translation>Traduce automáticamente y revisa el contenido antes de publicarlo</translation>
+    </message>
+    <message>
+      <source>Translate attachments</source>
+      <translation>Traduce los archivos adjuntos</translation>
+    </message>
+    <message>
+      <source>This content is translated with an automatic translation tool: the text may contain inaccurate information.</source>
+      <translation>Este contenido se ha traducido con una herramienta de traducción automática: el texto puede contener información inexacta.</translation>
+    </message>
+  </context>
+</TS>
---
+++ translations/por-BR/translation.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt-br">
+  <context>
+    <name>octranslate</name>
+    <message>
+      <source>Translator settings</source>
+      <translation>Configurações do tradutor automático</translation>
+    </message>
+    <message>
+      <source>A %locale% translation already exists for the selected content</source>
+      <translation>Já existe uma tradução em %locale% para o conteúdo selecionado</translation>
+    </message>
+    <message>
+      <source>Manually edit the current translation</source>
+      <translation>Editar manualmente a tradução atual</translation>
+    </message>
+    <message>
+      <source>Regenerate the translation automatically and edit the content for review</source>
+      <translation>Regenerar automaticamente a tradução e revisar o conteúdo antes de publicá-lo
+            </translation>
+    </message>
+    <message>
+      <source>Create a new %locale% translation for the selected content</source>
+      <translation>Crie uma nova tradução em %locale% para o conteúdo selecionado</translation>
+    </message>
+    <message>
+      <source>Automatically translate the content and publish it</source>
+      <translation>Traduzir automaticamente e publicar o conteúdo</translation>
+    </message>
+    <message>
+      <source>Automatically translate without publishing and open the content for review</source>
+      <translation>Traduzir automaticamente e revisar o conteúdo antes de publicá-lo</translation>
+    </message>
+    <message>
+      <source>Translate attachments</source>
+      <translation>Traduzir os arquivos anexados</translation>
+    </message>
+    <message>
+      <source>This content is translated with an automatic translation tool: the text may contain inaccurate information.</source>
+      <translation>Este conteúdo é traduzido com uma ferramenta de tradução automática: o texto pode conter informações imprecisas.</translation>
+    </message>
+  </context>
+</TS>
