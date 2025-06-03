ffe9ceb9d80e80d0b4ee0e2d8d05e72cd5be3765
Luca Realdi <luca.realdi@opencontent.it>
2025-06-03 11:42:09 +0200

    Add fre-FR translations

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+  <context>
+    <name>octranslate</name>
+    <message>
+      <source>Translator settings</source>
+      <translation>Paramètres du traducteur automatique</translation>
+    </message>
+    <message>
+      <source>A %locale% translation already exists for the selected content</source>
+      <translation>Une traduction en %locale% existe déjà pour le contenu sélectionné</translation>
+    </message>
+    <message>
+      <source>Manually edit the current translation</source>
+      <translation>Modifier manuellement la traduction actuelle</translation>
+    </message>
+    <message>
+      <source>Regenerate the translation automatically and edit the content for review</source>
+      <translation>Régénérer automatiquement la traduction et réviser le contenu avant de le publier
+            </translation>
+    </message>
+    <message>
+      <source>Create a new %locale% translation for the selected content</source>
+      <translation>Créer une nouvelle traduction en %locale% pour le contenu sélectionné</translation>
+    </message>
+    <message>
+      <source>Automatically translate the content and publish it</source>
+      <translation>Traduire automatiquement et publier le contenu</translation>
+    </message>
+    <message>
+      <source>Automatically translate without publishing and open the content for review</source>
+      <translation>Traduire automatiquement et réviser le contenu avant de le publier</translation>
+    </message>
+    <message>
+      <source>Translate attachments</source>
+      <translation>Traduire les fichiers joints</translation>
+    </message>
+    <message>
+      <source>This content is translated with an automatic translation tool: the text may contain inaccurate information.</source>
+      <translation>Ce contenu est traduit avec un outil de traduction automatique : le texte peut contenir des informations inexactes.</translation>
+    </message>
+  </context>
+</TS>
