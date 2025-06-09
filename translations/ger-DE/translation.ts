9513b1b72361d098dc31daa6d4563a168a75665d
Luca Realdi <luca.realdi@opencontent.it>
2025-06-09 08:42:09 +0200

    Update ger-DE translations

--- translations/ger-DE/translation.ts
+++ translations/ger-DE/translation.ts
@@ -1,45 +1,45 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.0">
-    <context>
-        <name>octranslate</name>
-        <message>
-            <source>Translator settings</source>
-            <translation>Übersetzereinstellungen</translation>
-        </message>
-        <message>
-            <source>A %locale% translation already exists for the selected content</source>
-            <translation>Für den ausgewählten Inhalt ist bereits eine %locale% Übersetzung vorhanden</translation>
-        </message>
-        <message>
-            <source>Manually edit the current translation</source>
-            <translation>Manuell die aktuelle Übersetzung bearbeiten</translation>
-        </message>
-        <message>
-            <source>Regenerate the translation automatically and edit the content for review</source>
-            <translation>Regenerieren Sie die Übersetzung automatisch und bearbeiten Sie den Inhalt zur Überprüfung
+<TS version="2.1" language="de">
+  <context>
+    <name>octranslate</name>
+    <message>
+      <source>Translator settings</source>
+      <translation>Übersetzereinstellungen</translation>
+    </message>
+    <message>
+      <source>A %locale% translation already exists for the selected content</source>
+      <translation>Für den ausgewählten Inhalt ist bereits eine %locale% Übersetzung vorhanden</translation>
+    </message>
+    <message>
+      <source>Manually edit the current translation</source>
+      <translation>Manuell die aktuelle Übersetzung bearbeiten</translation>
+    </message>
+    <message>
+      <source>Regenerate the translation automatically and edit the content for review</source>
+      <translation>Regenerieren Sie die Übersetzung automatisch und bearbeiten Sie den Inhalt zur Überprüfung
             </translation>
-        </message>
-        <message>
-            <source>Create a new %locale% translation for the selected content</source>
-            <translation>Erstellen Sie eine neue %locale% Übersetzung für den ausgewählten Inhalt</translation>
-        </message>
-        <message>
-            <source>Automatically translate the content and publish it</source>
-            <translation>Übersetzen Sie den Inhalt automatisch und veröffentlichen Sie ihn</translation>
-        </message>
-        <message>
-            <source>Automatically translate without publishing and open the content for review</source>
-            <translation>Übersetzen Sie automatisch ohne Veröffentlichung und öffnen Sie den Inhalt zur Überprüfung
+    </message>
+    <message>
+      <source>Create a new %locale% translation for the selected content</source>
+      <translation>Erstellen Sie eine neue %locale% Übersetzung für den ausgewählten Inhalt</translation>
+    </message>
+    <message>
+      <source>Automatically translate the content and publish it</source>
+      <translation>Übersetzen Sie den Inhalt automatisch und veröffentlichen Sie ihn</translation>
+    </message>
+    <message>
+      <source>Automatically translate without publishing and open the content for review</source>
+      <translation>Übersetzen Sie automatisch ohne Veröffentlichung und öffnen Sie den Inhalt zur Überprüfung
             </translation>
-        </message>
-        <message>
-            <source>Translate attachments</source>
-            <translation type="unfinished"/>
-        </message>
-        <message>
-            <source>This content is translated with an automatic translation tool: the text may contain inaccurate information.</source>
-            <translation>Dieser Inhalt wurde mit einem automatischen Übersetzungstool übersetzt: Der Text kann ungenaue Informationen enthalten.</translation>
-        </message>
-    </context>
+    </message>
+    <message>
+      <source>Translate attachments</source>
+      <translation></translation>
+    </message>
+    <message>
+      <source>This content is translated with an automatic translation tool: the text may contain inaccurate information.</source>
+      <translation></translation>
+    </message>
+  </context>
 </TS>
